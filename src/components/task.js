bbe4102190096065e1eb73ce0d6c727976289a23
Ismail Aleskerov <118177389+ismales@users.noreply.github.com>
2024-05-14 17:19:38 +0400

    add timer to task

--- src/components/task.js
+++ src/components/task.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-template */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { formatDistanceToNow } from 'date-fns';
@@ -5,7 +6,38 @@ import { formatDistanceToNow } from 'date-fns';
 import './task.css';
 
 export default class Task extends Component {
+  state = {
+    timerID: null,
+    min: 0,
+    sec: 0,
+  };
+
+  componentDidUpdate() {
+    const { sec } = this.state;
+    if (+sec === 60) {
+      // eslint-disable-next-line react/no-did-update-set-state
+      this.setState((prevState) => ({
+        min: prevState.min + 1,
+        sec: 0,
+      }));
+    }
+  }
+
+  onTimerPlay = () => {
+    this.state.timerID = setInterval(() => {
+      this.setState((prevState) => ({
+        sec: prevState.sec + 1,
+      }));
+    }, 1000);
+  };
+
+  onTimerPause = () => {
+    const { timerID } = this.state;
+    clearInterval(timerID);
+  };
+
   render() {
+    const { min, sec } = this.state;
     const { task, onDeleted, onToggleDone } = this.props;
     const { id, text, done, createdTime } = task;
     let classNames = '';
@@ -13,6 +45,7 @@ export default class Task extends Component {
     if (done) {
       classNames += ' completed';
       checked = true;
+      this.onTimerPause();
     }
 
     return (
@@ -21,10 +54,23 @@ export default class Task extends Component {
           <input id={id} className="toggle" type="checkbox" onChange={onToggleDone} checked={checked} />
           <label htmlFor={id}>
             <span className="description">{text}</span>
+            <span className="task-timer">
+              <button
+                type="button"
+                aria-label="Play"
+                className="icon-play icon-hover"
+                disabled={done}
+                onClick={this.onTimerPlay}
+              />
+              <button type="button" aria-label="Pause" className="icon-pause icon-hover" onClick={this.onTimerPause} />
+              {min < 10 ? '0' + min : min}:{sec < 10 ? '0' + sec : sec}
+            </span>
             <span className="created">created {formatDistanceToNow(createdTime, { includeSeconds: true })} ago</span>
+            <div className="task-buttons">
+              <button type="button" aria-label="Edit" className="icon icon-edit icon-hover" />
+              <button type="button" aria-label="Destoy" className="icon icon-destroy icon-hover" onClick={onDeleted} />
+            </div>
           </label>
-          <button type="button" aria-label="Edit" className="icon icon-edit" />
-          <button type="button" aria-label="Destoy" className="icon icon-destroy" onClick={onDeleted} />
         </div>
         <input type="text" className="edit" defaultValue={text} />
       </li>
